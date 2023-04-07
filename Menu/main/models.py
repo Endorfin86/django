32d02ef166f97b1f6ffa8788ab243ec1ad6ac17f
Endorfin86 <yae.rodionov@gmail.com>
2023-04-07 18:08:10 +0300

    edit main/models.py and main/apps.py

--- Menu/main/models.py
+++ Menu/main/models.py
@@ -17,7 +17,7 @@ class Items(MPTTModel):
     title = models.CharField('Название', max_length=50, unique=True)
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="Имя")
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children', verbose_name='Родитель')
-    menu = models.ForeignKey(Menu, on_delete=models.SET_NULL, verbose_name='Привязано к меню', blank=True, null=True)
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, verbose_name='Привязано к меню')
 
     def __str__(self):
         return self.title
